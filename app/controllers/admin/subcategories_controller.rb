84d5945e508d6d37d2b1a13a21b4904675d30bd8
ludovic guadagnin <guadagnin.ludovic@gmail.com>
2026-09-16 00:10:57 +0200

    feat(admin): redimensionnement des photos (colspan 1/2/3) depuis l'admin
    - photos_attributes autorise désormais colspan (même mécanisme qu'alt_text)
    - <select> par photo dans le formulaire d'édition
    - validation colspan (1..3) déjà présente sur Photo, aucune nouvelle validation nécessaire

--- app/controllers/admin/subcategories_controller.rb
+++ app/controllers/admin/subcategories_controller.rb
@@ -45,8 +45,8 @@ module Admin
           private
 
       def subcategory_params
-        # photos: [] retiré (géré par attach_photos) ; photos_attributes : édition d'alt_text uniquement
-        params.require(:subcategory).permit(:name, :description, photos_attributes: [:id, :alt_text])
+        # photos: [] retiré (géré par attach_photos) ; photos_attributes : édition alt_text + colspan
+        params.require(:subcategory).permit(:name, :description, photos_attributes: [:id, :alt_text, :colspan])
       end
 
       def uploaded_photo_files
