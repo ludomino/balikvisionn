da83875ec87932be51b75d3942035a02a46b59dd
ludovic guadagnin <guadagnin.ludovic@gmail.com>
2026-09-17 00:46:54 +0200

    fix: nettoyage sémantique HTML (html dupliqué, h1 manquant, h3 mal utilisé, div surnuméraire)

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -19,4 +19,26 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     get root_path
     assert_select "a[href='#{about_path}']"
   end
+
+  test "index displays exactly one link per category, correctly nested" do
+    get root_path
+    assert_select "a[href='#{category_path(@category)}']", count: 1
+  end
+
+  test "layout does not duplicate the <html> root tag" do
+    get root_path
+    assert_equal 1, response.body.scan("<html").size
+  end
+
+  test "index has a single top-level heading for the page" do
+    get root_path
+    assert_select "h1", count: 1
+  end
+
+  test "subcategory description is rendered as text, not as a heading" do
+    subcategory = Subcategory.create!(name: "Portugal", description: "Été 2024", category: @category)
+    get category_path(@category)
+    assert_select "h3", count: 0
+    assert_select "p", text: subcategory.description
+  end
 end
